796471b43039042dac4bcd1cf7bddca55978f518
Manouk <m.van.teeffelen@hotmail.com>
2020-06-16 11:31:25 +0200

    Ingredienten funcite gemaakt

--- CocktailApi/app/Http/Controllers/IngredientenController.php
+++ CocktailApi/app/Http/Controllers/IngredientenController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Ingredienten;
 
 class IngredientenController extends Controller
 {
-    //
+    public function getIngredientByCocktail($cocktailId) {
+      if (Ingredienten::where('cocktail_id', $cocktailId)->exists()) {
+        $ingredients = Ingredienten::where('cocktail_id', $cocktailId)->get()->toJson(JSON_PRETTY_PRINT);
+        return response($ingredients, 200);
+      } else {
+        return response()->json([
+              "message" => "No products found for the cocktail with cocktail id: " . $cocktailId
+            ], 404);
+      }
+    }
 }
--- CocktailApi/routes/web.php
+++ CocktailApi/routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/cocktail', 'CocktailController@index');
+
+Route::get('ingredient/by-cocktail-id/{cocktailId}', 'IngredientenController@getIngredientByCocktail');
+
 Route::get('/', function () {
     return view('welcome');
 });
