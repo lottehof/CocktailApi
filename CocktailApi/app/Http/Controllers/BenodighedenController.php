bb96be638e5dda7d81672e61211c67ba48fa84ea
Manouk <m.van.teeffelen@hotmail.com>
2020-06-16 11:40:11 +0200

    Functie voor instructies en benodigheden toegevoegd"

--- CocktailApi/app/Http/Controllers/BenodighedenController.php
+++ CocktailApi/app/Http/Controllers/BenodighedenController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Benodigheden;
 
 class BenodighedenController extends Controller
 {
-    //
+  public function getBenodighedenByCocktail($cocktailId) {
+    if (Benodigheden::where('cocktail_id', $cocktailId)->exists()) {
+      $benodigheden = Benodigheden::where('cocktail_id', $cocktailId)->get()->toJson(JSON_PRETTY_PRINT);
+      return response($benodigheden, 200);
+    } else {
+      return response()->json([
+            "message" => "No products found for the cocktail with cocktail id: " . $cocktailId
+          ], 404);
+    }
+  }
 }
--- CocktailApi/app/Http/Controllers/InstructiesController.php
+++ CocktailApi/app/Http/Controllers/InstructiesController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Instructies;
 
 class InstructiesController extends Controller
 {
-    //
+  public function getInstructiesByCocktail($cocktailId) {
+    if (Instructies::where('cocktail_id', $cocktailId)->exists()) {
+      $instructies = Instructies::where('cocktail_id', $cocktailId)->get()->toJson(JSON_PRETTY_PRINT);
+      return response($instructies, 200);
+    } else {
+      return response()->json([
+            "message" => "No products found for the cocktail with cocktail id: " . $cocktailId
+          ], 404);
+    }
+  }
 }
